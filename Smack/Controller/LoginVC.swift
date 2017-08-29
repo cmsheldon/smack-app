884d1b1364262f2dfd6712b5636d92aa61bfafd0
Christina Sheldon <christina.m.sheldon@gmail.com>
2017-08-29 16:59:51 -0600

    Added loginVC

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Christina Sheldon on 8/29/17.
+//  Copyright © 2017 CoolwaterDesignStudio. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Christina Sheldon on 8/29/17.
+//  Copyright © 2017 CoolwaterDesignStudio. All rights reserved.
+//
+
+import Foundation
+
+// segues
+
+let TO_LOGIN = "toLogin"
