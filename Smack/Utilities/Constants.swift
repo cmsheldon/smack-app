5db40dadd96ae6ef8f94a2399010494489ec556f
Christina Sheldon <christina.m.sheldon@gmail.com>
2017-08-31 20:47:31 -0600

    Integrated API, began set up for server

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,8 +8,23 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// url constants
+
+let BASE_URL = "https://appsmack.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
 // segues
 
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+// user defaultsFoundation
+
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
+
